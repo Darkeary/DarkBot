cf96c99a996a42bec28ae063bd4226dd7a4aa5ce
Darkeary <roman743@hotmail.com>
2021-05-10 05:47:19 +0300

    token moved to new file

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,11 +1,14 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import javax.security.auth.login.LoginException;
 
 public class main {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws LoginException {
 
-        String token = new String("f9f42478c6b8023e0728f1c63eb33babd15a98a93d57d47acd559312b8acfec11");
+        Token tokenClass = new Token();
+
+        String token = tokenClass.getToken();
 
         JDA jda = JDABuilder.createDefault(token).build();
 
