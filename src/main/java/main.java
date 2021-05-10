3160f4ce73220b8dc5c2ef9434bb701de5bb0fae
Darkeary <roman743@hotmail.com>
2021-05-10 05:31:13 +0300

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,14 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class main {
+
+    public static void main(String[] args) throws Exception{
+
+        String token = new String("f9f42478c6b8023e0728f1c63eb33babd15a98a93d57d47acd559312b8acfec11");
+
+        JDA jda = JDABuilder.createDefault(token).build();
+
+
+    }
+}
